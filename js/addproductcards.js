9ade7c4d05cf9d68235fb53cb71d301038af9035
martin <loveablelobster@fastmail.fm>
2021-04-07 20:55:19 +0200

    index and header adjustments

--- js/addproductcards.js
+++ js/addproductcards.js
@@ -1,83 +1,57 @@
 var addProductCards = function() {
     document.getElementsByClassName('article-content')[0].innerHTML +=
     '<div class="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet">\
-        <h4>All game types:</h4>\
+        <h4>Læs mere:</h4>\
         <div class="mdl-grid index-games-grid">\
             <div class="demo-card-wide mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-phone is-relative">\
                 <div class="mdl-card__title linercard1">\
-                    <h2 class="mdl-card__title-text">Standard</h2>\
+                    <h2 class="mdl-card__title-text">Værdigrundlag</h2>\
                 </div>\
                 <div class="mdl-card__supporting-text">\
-                    Game for new employees in container lines, freight forwarders, ports, terminals and supply chain managers.\
+                    Bla bla bla\
                 </div>\
                 <div class="mdl-card__actions in-bottom">\
-                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="standard-game.html">\
-                        Get Started\
-                    </a>\
-                </div>\
-            </div>\
-            <div class="demo-card-wide mdl-card mdl-shadow--2dp mdl-cell mdl-cell mdl-cell--4-col mdl-cell--4-col-phone is-relative">\
-                <div class="mdl-card__title linercard2">\
-                    <h2 class="mdl-card__title-text">Executive</h2>\
-                </div>\
-                <div class="mdl-card__supporting-text">\
-                    Game for board members and senior managers in container shipping lines, freight forwarders, ports and terminals.\
-                </div>\
-                <div class="mdl-card__actions in-bottom">\
-                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="executive-game.html">\
-                        Get Started\
+                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="vaerdigrundlag.html">\
+                        Læs mere\
                     </a>\
                 </div>\
             </div>\
             <div class="demo-card-wide mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-phone is-relative">\
                 <div class="mdl-card__title linercard3">\
-                    <h2 class="mdl-card__title-text">Support Functions</h2>\
+                    <h2 class="mdl-card__title-text">En dag i praksis</h2>\
                 </div>\
                 <div class="mdl-card__supporting-text">\
-                    Ideal for shipping employees in IT, Finance, HR, Communication, Marketing, Procurement, Project Managers and Process Consultants.\
+                    Bla bla bla\
                 </div>\
                 <div class="mdl-card__actions in-bottom">\
-                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="support-functions-game.html">\
-                        Get Started\
+                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="en-dag-i-praksis.html">\
+                        Læs mere\
                     </a>\
                 </div>\
             </div>\
             <div class="demo-card-wide mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-phone is-relative">\
                 <div class="mdl-card__title linercard4">\
-                    <h2 class="mdl-card__title-text">Teambuilding</h2>\
+                    <h2 class="mdl-card__title-text">Lærer- og forældreengagement</h2>\
                 </div>\
                 <div class="mdl-card__supporting-text">\
-                    Teambuilding well suited for newly formed departments or mixed project teams.\
+                    Bla bla bla\
                 </div>\
                 <div class="mdl-card__actions in-bottom">\
-                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="teambuilding-game.html">\
-                        Get Started\
+                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="laerer-foraeldre.html">\
+                        Læs mere\
                     </a>\
                 </div>\
             </div>\
-            <div class="demo-card-wide mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-phone is-relative">\
-                <div class="mdl-card__title linercard5">\
-                    <h2 class="mdl-card__title-text">For Ports</h2>\
+            <div class="demo-card-wide mdl-card mdl-shadow--2dp mdl-cell mdl-cell mdl-cell--4-col mdl-cell--4-col-phone is-relative">\
+                <div class="mdl-card__title linercard2">\
+                    <h2 class="mdl-card__title-text">Vedtægter</h2>\
                 </div>\
                 <div class="mdl-card__supporting-text">\
-                    For port personnel with a need to better understand the container shipping lines.\
+                    Bla bla bla\
                 </div>\
                 <div class="mdl-card__actions in-bottom">\
-                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="ports-game.html">\
-                        Get Started\
-                    </a>\
-                </div>\
-            </div>\
-            <div class="demo-card-wide mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-phone is-relative">\
-                <div class="mdl-card__title  linercard6">\
-                    <h2 class="mdl-card__title-text">Advanced</h2>\
-                </div>\
-                <div class="mdl-card__supporting-text">\
-                    Tailor-made sessions matching specific skills and requirements.\
-                </div>\
-                <div class="mdl-card__actions in-bottom">\
-                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="advanced-game.html">\
-                Get Started\
+                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="vedtaegter.html">\
+                        Læs mere\
                     </a>\
                 </div>\
             </div>\
